3b7d3433d005979a61d6138bb21cceddf0c03eb0
pepaSuli <papp.peter@ady-nagyatad.hu>
2022-09-26 09:40:09 +0200

    üzemanyag

---
+++ ConsoleApp1/Uzemanyag/Program.cs
@@ -0,0 +1,62 @@
+﻿
+feladat f = new feladat();
+
+class feladat
+{
+    List<adatok> arvaltozas = new List<adatok>();
+    public feladat()
+    {
+        f2();
+        f3();
+        f4();
+    }
+    void f4()
+    {
+        int minK = arvaltozas[0].kulonbseg();
+        for (int i = 0; i < arvaltozas.Count; i++)
+        {
+            if (minK > arvaltozas[i].kulonbseg())
+            {
+                minK=arvaltozas[i].kulonbseg();
+            }
+        }
+
+        Console.WriteLine("4. feladat: A legkisebb különbség: {0}",minK);
+    }
+    void f3()
+    {
+        Console.WriteLine("3. feladat: Változások száma {0}",arvaltozas.Count);
+    }
+
+    void f2()
+    {
+        string[] sorok = File.ReadAllLines("uzemanyag.txt");
+        for (int i = 0; i < sorok.Length; i++)
+        {
+            arvaltozas.Add(new adatok(sorok[i]));
+        }
+    }
+}
+
+
+
+class adatok
+{
+    public string datum;
+    public int benzin, gazolaj;
+
+    public adatok(string sor)
+    {
+        string[] vag=sor.Split(';');
+        datum=vag[0];
+        benzin= Convert.ToInt32(vag[1]);
+        gazolaj= Convert.ToInt32(vag[2]);
+    }
+
+
+    public int kulonbseg()
+    {
+        return Math.Abs(benzin - gazolaj);
+    }
+
+}
--- cbradio/cbradio/Program.cs
+++ cbradio/cbradio/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,6 +11,58 @@ namespace cbradio
     {
         static void Main(string[] args)
         {
+            feladat f = new feladat();
+        }
+    }
+
+    class feladat
+    {
+        List<adatok> adasok = new List<adatok>();
+        public feladat()
+        {
+            f2();
+            f3();
+            f4();
+        }
+        void f4()
+        {
+            for (int i = 0; i < adasok.Count; i++)
+            {
+                if (adasok[i].adasDb==4)
+                {
+                    Console.WriteLine("4. feladat: Volt 4 adás");
+                    break;
+                }
+            }
+
+        }
+        void f2()
+        {
+            string[] sorok = File.ReadAllLines("cb.txt");
+            for (int i = 1; i < sorok.Length; i++)
+            {
+                adasok.Add(new adatok(sorok[i]));
+            }
+
+        }
+        void f3()
+        {
+            Console.WriteLine("3. feladat: {0}",adasok.Count);
+        }
+    }
+
+
+    class adatok
+    {
+        public int ora, perc, adasDb;
+        public string nev;
+        public adatok(string sor)
+        {
+            string[] vag = sor.Split(';');
+            ora = int.Parse(vag[0]);
+            perc = Convert.ToInt32(vag[1]);
+            adasDb=Convert.ToInt32(vag[2]);
+            nev= vag[3];
         }
     }
 }
